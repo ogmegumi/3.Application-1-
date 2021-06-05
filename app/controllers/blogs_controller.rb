3b7f7f8ae91ebdd863c8ac4db6ef13cc08d100fa
ogmegumi <ogmegumi@gmail.com>
2021-06-05 12:46:24 +0000

    [Update]3-4 Blog部分を書き換えよう ブログ投稿時に自動更新

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,14 +3,15 @@ class BlogsController < ApplicationController
   end
 
   def index
+    @blogs = Blog.all
   end
 
   def new
     @blog = Blog.new
   end
 
-  def creat
-    blog = Blog.new(blog_params)
+  def create
+   blog = Blog.new(blog_params)
     blog.save
     redirect_to blogs_path
   end
